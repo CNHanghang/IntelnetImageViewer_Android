5dd411f2dc2a3750cd3cc3726eb786eb993fa383
HANGHANG <hcszheng@163.com>
2018-04-06 15:46:07 +0800

    add webView

---
+++ app/src/main/java/com/example/hcszh/pickwebimage/WebViewActivity.java
@@ -0,0 +1,21 @@
+package com.example.hcszh.pickwebimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webview = (WebView)findViewById(R.id.web_view);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://www.btbtdy.com/");
+
+    }
+}
