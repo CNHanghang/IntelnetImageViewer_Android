fb48dc5bbbf585f04ae82d3f0675cfca050f7f4e
HANGHANG <hcszheng@163.com>
2018-04-06 16:31:04 +0800

    test ui

--- app/src/main/java/com/example/hcszh/pickwebimage/WebViewActivity.java
+++ app/src/main/java/com/example/hcszh/pickwebimage/WebViewActivity.java
@@ -2,20 +2,46 @@ package com.example.hcszh.pickwebimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.view.KeyEvent.KEYCODE_BACK;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    private WebView webview;
+    private EditText editTextHttp;
+    private Button buttonGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        WebView webview = (WebView)findViewById(R.id.web_view);
+//        editTextHttp = (EditText)findViewById(R.id.edittext_http);
+//        buttonGo = (Button)findViewById(R.id.button_go);
+//        buttonGo.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+        webview = (WebView)findViewById(R.id.web_view);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("http://www.btbtdy.com/");
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
